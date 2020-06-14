26c5ae2af2bdb82f67b0f415012172cdaf64648d
LeverestFish <1800016611@pku.edu.cn>
2020-06-14 20:08:09 +0800

    热门推荐OK

--- scripts/index.js
+++ scripts/index.js
@@ -16,7 +16,6 @@ function makeAlert(type, message) {
 
 
 let stockInfo;
-let industryList;
 
 // 获取公司信息
 function getStockInfo() {
@@ -34,18 +33,6 @@ function getStockInfo() {
     });
     console.log('hi');
     return stockIdList;
-}
-
-function getIndustryList() {
-    $.ajax({
-        url: '/static/industry.json',
-        async: false,
-        success: function (infoData) {
-            industryList = infoData;
-            console.log("获取行业列表成功");
-        }
-    });
-    return industryList;
 }
 
 // 用来查找ID对应名称
@@ -80,24 +67,6 @@ $(function () {
             console.log(item + ' ' + code2Name(item)['name']);
         }
     });
-
-    $("#industry-input").typeahead({
-        source: getIndustryList(), // 数据源
-        items: 8,           //最多显示个数
-        delay: 0,         //延迟时间
-        //这里一定要return，否则选中不显示，外加调用display的时候null reference错误。
-        updater: function (item) {
-            return item;
-        },
-        //返回选中的字符串
-        displayText: function (item) {
-            return item;
-        },
-        //选择项之后的事件，item是当前选中的选项
-        afterSelect: function (item) {
-            console.log(item);
-        }
-    });
 })
 
 // 处理按钮点击
@@ -115,18 +84,6 @@ $(function () {
         }
         createChart(queryStr, 'stock');
     });
-
-    $('#industry').find('#industry-search').click(function () {
-        let queryStr = $('#industry').find('#industry-input').text();
-        console.log("检索ID为：");
-        console.log(queryStr);
-        if (queryStr in industryList) {
-            // alert("行业名称不正确！")
-            makeAlert('stock', '行业名称不正确！');
-            return;
-        }
-        createChart(queryStr, 'industry');
-    });
 })
 
 // 热门推荐搜索
@@ -135,9 +92,9 @@ $(function () {
         let queryStr = $(this).text().substring(0, 8);
         createChart(queryStr, 'stock');
     });
-    $('.industry-recommend').click(function () {
-        let queryStr = $(this).text();
-        createChart(queryStr, 'industry');
+    $('.hot-recommend').click(function () {
+        let queryStr = $(this).text().substring(0, 8);
+        createChart(queryStr, 'hot');
     });
 });
 
@@ -146,12 +103,12 @@ $(function () {
 function createChart(queryStr, type) {
     let node;
     if (type === 'stock') node = $('#stock');
-    else node = $('#industry');
+    else node = $('#hot');
 
     $.ajax({
         url: '/api/stockSenti.php',
         type: "get",
-        data: {'queryStr': queryStr, 'type': type},
+        data: {'queryStr': queryStr, 'type': 'stock'},
         dataType: "json",
         beforeSend: function () {
             node.find('.panel-heading').text('查询中...请耐心等待...')
@@ -166,27 +123,15 @@ function createChart(queryStr, type) {
             console.log('查询成功');
             console.log(sentiData);
             let myStockInfo;
-            if (type === 'stock') {
-                myStockInfo = code2Name(queryStr);
-                node.find('.panel-heading').html(`${myStockInfo['name']} [${queryStr}]<span style="padding: 10px;"></span>行业：<button class="btn btn-success btn-xs" onclick="jumpToIndustry($(this).text())">${myStockInfo['industry']}</button>`);
-            } else {
-                node.find('.panel-heading').html(`行业：${queryStr}`);
-            }
+            myStockInfo = code2Name(queryStr);
+            node.find('.panel-heading').html(`${myStockInfo['name']} [${queryStr}]<span style="padding: 10px;"></span>行业：${myStockInfo['industry']}`);
             let panelBody = node.find(`.panel-body #${type}-chart`);
             panelBody.text('');
             panelBody.css('height', '450px');
-            if (type === 'stock') Highcharts.stockChart('stock-chart', stockChartConfig(queryStr, myStockInfo['name'], sentiData));
-            else Highcharts.stockChart('industry-chart', industryChartConfig(queryStr, sentiData));
+            Highcharts.stockChart(`${type}-chart`, stockChartConfig(queryStr, myStockInfo['name'], sentiData));
         },
         error: function () {
             makeAlert(type, '未知错误，请联系管理员。');
         }
     });
-}
-
-
-function jumpToIndustry(queryStr) {
-    // alert('clicked');
-    $('#industry-tab').click();
-    createChart(queryStr, 'industry');
 }
